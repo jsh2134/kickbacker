ead7932323f4b1878e58ddc7afabdeae652710c8
Jeff Hull <jsh2134@gmail.com>
2012-09-02 10:57:07 +0200

    fixed deployment with correct paths and better usage of virtualenv

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -17,21 +17,49 @@ KB_BUCKET = 's3://kickbacker/'
 HOSTS = {	
 			'web1' : { 'user':'ec2-user',
 						'host': MAIN_IP,
-						'key': os.path.join(USER_HOME, '.ssh', aws_defaults['keypair']['file'])
+						'key': os.path.join(USER_HOME, '.ssh',\
+								 aws_defaults['keypair']['file']),
+						'dir': '/home/kickbacker/kickbacker-env',
+						'env': '/home/kickbacker/kickbacker-env/bin/activate',
+						'deploy_user': 'kickbacker',
 			},
 		}
 
+
+
+
+
 env.key_filename = HOSTS['web1']['key']
 
-def host(hostname):
-	env.hosts.append("%s@%s" % ( HOSTS[hostname]['user'], \
-							HOSTS[hostname]['host']))
+def virtualenv(command, use_sudo=True):
+	""" Execute a command with sudo under venv """
+	with cd(env.directory):
+		sudo('source ' + env.activate + ' && ' + command)#, user=env.deploy_user)
+		
 
-def ssh(host):
-	local('ssh -i %s %s@%s' % ( HOSTS[host]['key'],
-								HOSTS[host]['user'],
-								HOSTS[host]['host'],
+
+def host(hostname):
+	print "Setting host to %s" % hostname
+	env.host_string = "%s@%s" % ( HOSTS[hostname]['user'], \
+									HOSTS[hostname]['host'])
+	
+	# Activate VirtualEnv
+	if 'env' in HOSTS[hostname]:
+		env.directory = HOSTS[hostname]['dir']
+		env.activate = HOSTS[hostname]['env']
+		env.deploy_user = HOSTS[hostname]['deploy_user']
+
+
+def ssh(hostname):
+	
+	# Set Host
+	host(hostname)
+
+	local('ssh -i %s %s@%s' % ( HOSTS[hostname]['key'],
+								HOSTS[hostname]['user'],
+								HOSTS[hostname]['host'],
 								) )
+
 
 def deploy_web():
 
@@ -42,7 +70,10 @@ def deploy_web():
 	local("ssh-keygen -R %s" % (instance.ip_address) )
 
 	# Set Env Host String
-	env.host_string = "ec2-user@%s" % (instance.ip_address) 
+	#env.host_string = "ec2-user@%s" % (instance.ip_address) 
+	# In this case web1 is the host
+	#TODO make this better
+	host('web1')
 
 	# Install Web
 	install_web()
@@ -64,7 +95,7 @@ def install_web():
 		sudo('mkdir %s' % remote_code_dir)
 
 	# Install packages with yum
-	sudo('yum install -y python-devel gcc nginx')
+	sudo('yum install -y python-devel gcc nginx make')
 
 	# Install s3cmd tools
 	#install_s3cmd(remote_home_dir)
@@ -74,48 +105,51 @@ def install_web():
 	run('tar xvfz pip-1.0.tar.gz')
 	sudo('cd pip-1.0 && python setup.py install')
 
-	#run('echo $PYTHONPATH')
-
 	# Install virtualenv 
 	sudo('pip install virtualenv')
 	venv_name = '%s-env' % user
 	venv = os.path.join(remote_home_dir, venv_name)
 	sudo('virtualenv --no-site-packages %s' % venv)
 
-	run('echo $PYTHONPATH')
-
 	# Activate Virtual Env
-	#sudo('source %s/bin/activate' % venv)
+	virtualenv('source %s/bin/activate' % venv)
 	
 	# Install python requirements
 	put('requirements.txt', remote_home_dir, use_sudo=True)
-	sudo('%s/bin/pip install -r %s/requirements.txt' % (venv, remote_home_dir))
+	virtualenv('pip install -r %s/requirements.txt' % (remote_home_dir))
 	
 	# Install Nginx Config
 	put('nginx.conf', '/etc/nginx/nginx.conf', use_sudo=True)
 
 	# Install Supervisor Config
 	sudo('mkdir %s/etc/' % venv)
+	sudo('mkdir /var/log/supervisord/')
 	put('supervisord.conf', '%s/etc/supervisord.conf' % venv, use_sudo=True)
 
 	# Update python encoding
 	pyv = run("""python -c 'import sys; print "%s.%s" % (sys.version_info[0], sys.version_info[1])'""")
 	sudo("""sed -i 's/encoding.[^!]*=.*\"ascii\"/encoding=\"utf8\"/' %s/lib/python%s/site.py""" % (venv, pyv) )
 
+	# Make Kickbacker log
+	sudo('touch /var/log/kickbacker.log')
+	sudo('chown kickbacker:kickbacker /var/log/kickbacker.log')
+
+	# Deploy Code
 	update_code(HOME_DIR, remote_code_dir)
 
 	# Install Redis
 	install_redis()
 
-	start_app(remote_code_dir)
+	start_app()
 
-def start_app(code_dir):
+def start_app():
 	# start nginx
 	sudo('service nginx start')
 
 	# start supervisor
-	sudo('/home/kickbacker/kickbacker-env/bin/supervisord', pty=True)
-	sudo('supervisorctl start redis celery kickbacker')
+	virtualenv('supervisord')
+
+	#virtualenv('supervisorctl start redis celery kickbacker')
 
 	# start redis
 	#sudo("service redis start")
@@ -130,7 +164,8 @@ def start_app(code_dir):
 
 def update_code(plocal, premote):
 	with lcd(plocal):
-		local('git archive --format=tar.gz --output %s HEAD' % os.path.join(TMP_PATH, 'app.tar.gz'))
+		local('git archive --format=tar.gz --output %s HEAD' % \
+									os.path.join(TMP_PATH, 'app.tar.gz'))
 	put( os.path.join(TMP_PATH,'app.tar.gz'), os.path.join(premote, 'app.tar.gz'), use_sudo=True)
 	sudo('gunzip %s' % os.path.join(premote, 'app.tar.gz'))
 	with cd(premote):
@@ -140,22 +175,22 @@ def install_redis():
 
 	with settings(warn_only=True):
 		sudo('useradd redis', pty=True)
-		sudo('mkdir %s' % os.path.join('var','run','redis'), pty=True)
-		sudo('mkdir %s' % os.path.join('var','lib','redis'), pty=True)
-		sudo('mkdir %s' % os.path.join('var','log','redis'), pty=True)
-		sudo('mkdir %s' % os.path.join('etc','redis'), pty=True)
+		sudo('mkdir %s' % os.path.join(os.path.sep, 'var','run','redis'), pty=True)
+		sudo('mkdir %s' % os.path.join(os.path.sep, 'var','lib','redis'), pty=True)
+		sudo('mkdir %s' % os.path.join(os.path.sep, 'var','log','redis'), pty=True)
+		sudo('mkdir %s' % os.path.join(os.path.sep, 'etc','redis'), pty=True)
 		sudo('chown redis:redis /var/log/redis /var/lib/redis /var/run/redis')
 
 	put('redis/redis.conf', '/etc/redis/redis.conf', use_sudo=True)
-	put('redis/redis.init', '/etc/rc.d/init.d/redis', use_sudo=True)
+	put('redis/redis.init', '/etc/rc.d/init.d/redis', use_sudo=True, mode=751)
 
 	sudo('wget http://download.redis.io/redis-stable.tar.gz', pty=True)
 	sudo('tar xvzf redis-stable.tar.gz', pty=True)
 	with cd('redis-stable'):
 		sudo('make', pty=True)
 		sudo('make install', pty=True)
-		sudo('cp redis-server /usr/local/bin/')
-		sudo('cp redis-cli /usr/local/bin/')
+		sudo('cp src/redis-server /usr/local/bin/')
+		sudo('cp src/redis-cli /usr/local/bin/')
 
 	sudo('service redis start', pty=True)
 
